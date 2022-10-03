c2f25d15cf762c4c6f0165c866c98b27fae950a1
Martin Andonegui <tintinando@gmail.com>
2022-10-03 17:57:59 -0300

    Iniciando

---
+++ main.js
@@ -0,0 +1,27 @@
+/* El presente código simula una carga de datos para posterior registro en una base de datos */
+let numArtefacto = -1; // inicializado a un valor inexistente
+let registroArtefacto = "";
+
+const solicitarNum = (alert) => {
+  // solicita un número al usuario con mensaje opcional, y lo pasa a Float
+  let mensaje = alert;
+  if (alert === undefined) mensaje = 'Ingrese un número';
+  return parseFloat(prompt(mensaje));
+};
+
+const adicionArtefacto = (numArtefacto) => {
+  // A partir del código devuelve un string con el registro CSV
+  const nomArtefacto = prompt("Ingrese el nombre del artefacto");
+  const consumoArtefacto = prompt("Ingrese el consumo del artefacto");
+  return numArtefacto + "," + nomArtefacto + "," + consumoArtefacto;
+};
+
+while (numArtefacto !== 0) {
+  numArtefacto = solicitarNum("Ingrese número de artefacto. 0 para salir");
+  if (numArtefacto !== 0) {
+    registroArtefacto = adicionArtefacto(numArtefacto);
+  }
+}
+console.log(registroArtefacto);
+
+if (registroArtefacto !== "") alert("El último ingresado fue " + registroArtefacto);
